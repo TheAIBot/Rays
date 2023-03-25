bfb7130490055a4ffa914955aca6c9ccd4769845
TheAIBot <andreasuranus@hotmail.com>
2023-03-25 01:23:47 +0100

    Added a new scene factory and a scene that just displays static content

---
+++ Rays.Scenes/OnlyStaticContentScene.cs
@@ -0,0 +1,33 @@
+﻿using Rays.Polygons;
+using System.Numerics;
+
+namespace Rays.Scenes;
+
+internal sealed class OnlyStaticContentScene : IScene
+{
+    private readonly IPolygonDrawer _polygonDrawer;
+    private readonly List<Wall> _staticWalls;
+
+    public OnlyStaticContentScene(IPolygonDrawer polygonDrawer, List<Wall> staticWalls)
+    {
+        _polygonDrawer = polygonDrawer;
+        _staticWalls = staticWalls;
+    }
+
+    public async Task RenderAsync()
+    {
+        var ray = new Ray(new Vector2(70, 90), Vector2.Normalize(new Vector2(-1, -1.5f)));
+        List<Line> lines = Ray.GetRayPath(ray, 9, _staticWalls);
+
+        await _polygonDrawer.ClearAsync();
+        foreach (var wall in _staticWalls)
+        {
+            await _polygonDrawer.DrawAsync(wall);
+        }
+        foreach (var line in lines)
+        {
+            await _polygonDrawer.DrawAsync(line);
+        }
+        await _polygonDrawer.RenderAsync();
+    }
+}
---
+++ Rays.Scenes/TriangleRectangleSceneFactory.cs
@@ -0,0 +1,33 @@
+﻿using Rays.Polygons;
+using System.Numerics;
+using Rectangle = Rays.Polygons.Rectangle;
+
+namespace Rays.Scenes;
+
+public sealed class TriangleRectangleSceneFactory : ISceneFactory
+{
+    public IScene Create(IPolygonDrawer polygonDrawer)
+    {
+        var triangle = new Triangle(new Vector2(20, 20), new Vector2(40, 40), new Vector2(60, 20));
+        var rectangle1 = new Rectangle(new Vector2(80, 10), new Vector2(20, 0), new Vector2(0, 20));
+        var rectangle2 = new Rectangle(new Vector2(30, 60), new Vector2(20, 0), new Vector2(0, 10));
+
+        var bottomWall = new Wall(new Line(new Vector2(0, 0), new Vector2(polygonDrawer.Size.X - 1, 0)), new Vector2(0, 1));
+        var topWall = new Wall(new Line(new Vector2(0, polygonDrawer.Size.Y - 1), new Vector2(polygonDrawer.Size.X - 1, polygonDrawer.Size.Y - 1)), new Vector2(0, -1));
+        var leftWall = new Wall(new Line(new Vector2(0, 0), new Vector2(0, polygonDrawer.Size.Y - 1)), new Vector2(1, 0));
+        var rightWall = new Wall(new Line(new Vector2(polygonDrawer.Size.X - 1, 0), new Vector2(polygonDrawer.Size.X - 1, polygonDrawer.Size.Y - 1)), new Vector2(-1, 0));
+
+        var walls = new List<Wall>
+        {
+            bottomWall,
+            topWall,
+            leftWall,
+            rightWall,
+        };
+        walls.AddRange(triangle.GetAsWalls());
+        walls.AddRange(rectangle1.GetAsWalls());
+        walls.AddRange(rectangle2.GetAsWalls());
+
+        return new OnlyStaticContentScene(polygonDrawer, walls);
+    }
+}
