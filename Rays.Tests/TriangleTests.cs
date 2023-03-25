1165fb82f51d49a0264e85b79892250f57aad8b0
TheAIBot <andreasuranus@hotmail.com>
2023-03-25 23:02:47 +0100

    Fix triangle test by making the correct normals for it

--- Rays.Tests/TriangleTests.cs
+++ Rays.Tests/TriangleTests.cs
@@ -11,11 +11,17 @@ public sealed class TriangleTests
         var top = new Vector2(0, 1);
         var bottomLeft = new Vector2(-1, 0);
         var bottomRight = new Vector2(1, 0);
+        var rightLine = new Line(bottomRight, top);
+        var leftLine = new Line(top, bottomLeft);
+        var bottomLine = new Line(bottomLeft, bottomRight);
+        var rightNormal = Vector2.Normalize(new Vector2(-rightLine.Direction.Y, rightLine.Direction.X));
+        var leftNormal = Vector2.Normalize(new Vector2(-leftLine.Direction.Y, leftLine.Direction.X));
+        var bottomNormal = Vector2.Normalize(new Vector2(-bottomLine.Direction.Y, bottomLine.Direction.X));
         var expectedWalls = new[]
         {
-            new Wall(new Line(bottomRight, top), Vector2.Zero),
-            new Wall(new Line(top, bottomLeft), Vector2.Zero),
-            new Wall(new Line(bottomLeft, bottomRight), Vector2.Zero)
+            new Wall(rightLine, rightNormal),
+            new Wall(leftLine, leftNormal),
+            new Wall(bottomLine, bottomNormal)
         };
         var triangle = new Triangle(top, bottomLeft, bottomRight);
 
