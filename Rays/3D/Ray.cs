6cc55f243990dcb4b68137fcec856807c47d5e45
TheAIBot <andreasuranus@hotmail.com>
2023-04-06 18:57:12 +0200

    Added 3d triangle and ray

---
+++ Rays/3D/Ray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Numerics;
+using System.Runtime.Intrinsics;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rays._3D;
+
+public sealed record class Ray(Vector3 Start, Vector3 Direction);
---
+++ Rays/3D/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Rays._3D;
+
+public readonly record struct Triangle(Vector3 CornerA, Vector3 CornerB, Vector3 CornerC)
+{
+    // https://stackoverflow.com/a/42752998
+    public bool TryGetIntersection(Ray ray, out Vector3 intersection)
+    {
+        Vector3 E1 = CornerB - CornerA;
+        Vector3 E2 = CornerC - CornerA;
+        Vector3 N = Vector3.Cross(E1, E2);
+        float det = -Vector3.Dot(ray.Direction, N);
+        float invdet = 1.0f / det;
+        Vector3 AO = ray.Start - CornerA;
+        Vector3 DAO = Vector3.Cross(AO, ray.Direction);
+        float u = Vector3.Dot(E2, DAO) * invdet;
+        float v = -Vector3.Dot(E1, DAO) * invdet;
+        float t = Vector3.Dot(AO, N) * invdet;
+
+        intersection = ray.Start + t * ray.Direction;
+        return (det >= 1e-6 && t >= 0.0 && u >= 0.0 && v >= 0.0 && (u + v) <= 1.0);
+    }
+}
