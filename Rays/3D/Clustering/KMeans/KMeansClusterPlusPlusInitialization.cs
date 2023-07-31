fdbccb236067c5177993291efc501f3876384b2b
TheAIBot <andreasuranus@hotmail.com>
2023-07-31 06:00:09 +0200

    No need to shuffle the whole array every time. Only shuffle chunks of it as necessary

--- Rays/3D/Clustering/KMeans/KMeansClusterPlusPlusInitialization.cs
+++ Rays/3D/Clustering/KMeans/KMeansClusterPlusPlusInitialization.cs
@@ -25,18 +25,34 @@ public sealed class KMeansClusterPlusPlusInitialization : IKMeansClusterInitiali
         {
             float totalDistance = UpdateBestClusterItemDistancesAndGetDistanceSum(clusters[i - 1], items, bestClusterItemDistances);
 
-            Shuffle(availableItemIndexes);
-
             float targetDistance = _random.NextSingle() * totalDistance;
             float cumulativeDistance = 0;
-            int chosenSampleIndex = 0;
+            int chosenAvailableItemIndex = -1;
 
-            for (int sampleIndex = 0; sampleIndex < availableItemIndexes.Length; sampleIndex++)
+            // Apparently it is more efficient to shuffle chunks of the array
+            // instead of shuffling it one index at a time in the inner loop.
+            // No idea why that is the case though but that is why this code
+            // works in chunks.
+            const int shuffleChunkSize = 256;
+            int shuffleChunkCount = (availableItemIndexes.Length + (shuffleChunkSize - 1)) / shuffleChunkSize;
+            for (int shuffleChunkIndex = 0; shuffleChunkIndex < shuffleChunkCount; shuffleChunkIndex++)
             {
-                cumulativeDistance += bestClusterItemDistances[availableItemIndexes[sampleIndex]];
-                if (cumulativeDistance >= targetDistance)
+                int startIndex = shuffleChunkIndex * shuffleChunkSize;
+                int endIndex = Math.Min(startIndex + shuffleChunkSize, availableItemIndexes.Length);
+                PartialYateShuffle(availableItemIndexes, startIndex, endIndex);
+
+                for (int sampleIndex = startIndex; sampleIndex < endIndex; sampleIndex++)
                 {
-                    chosenSampleIndex = sampleIndex;
+                    cumulativeDistance += bestClusterItemDistances[availableItemIndexes[sampleIndex]];
+                    if (cumulativeDistance >= targetDistance)
+                    {
+                        chosenAvailableItemIndex = sampleIndex;
+                        break;
+                    }
+                }
+
+                if (chosenAvailableItemIndex != -1)
+                {
                     break;
                 }
             }
@@ -45,9 +61,9 @@ public sealed class KMeansClusterPlusPlusInitialization : IKMeansClusterInitiali
             {
                 Console.WriteLine($"Cluster: {i}/{clusterCount - 1}");
             }
-            clusters.Add(new KMeansCluster<T>(items, items.Positions[availableItemIndexes[chosenSampleIndex]]));
+            clusters.Add(new KMeansCluster<T>(items, items.Positions[availableItemIndexes[chosenAvailableItemIndex]]));
 
-            RemoveIndex(ref availableItemIndexes, chosenSampleIndex);
+            RemoveIndex(ref availableItemIndexes, chosenAvailableItemIndex);
         }
 
         timer.Stop();
@@ -55,9 +71,9 @@ public sealed class KMeansClusterPlusPlusInitialization : IKMeansClusterInitiali
         return clusters.ToArray();
     }
 
-    private static void RemoveIndex(ref Span<int> availableItemIndexes, int indexToRmove)
+    private static void RemoveIndex(ref Span<int> availableItemIndexes, int indexToRemove)
     {
-        availableItemIndexes[indexToRmove] = availableItemIndexes[availableItemIndexes.Length - 1];
+        availableItemIndexes[indexToRemove] = availableItemIndexes[availableItemIndexes.Length - 1];
         availableItemIndexes = availableItemIndexes.Slice(0, availableItemIndexes.Length - 1);
     }
 
@@ -81,16 +97,17 @@ public sealed class KMeansClusterPlusPlusInitialization : IKMeansClusterInitiali
         return distanceSum;
     }
 
+    // Partial implementation of
     // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-    private void Shuffle<T>(Span<T> toShuffle)
+    private void PartialYateShuffle<T>(Span<T> toShuffle, int index, int endIndex)
     {
-        for (int i = 0; i < toShuffle.Length; i++)
+        for (int i = index; i < endIndex; i++)
         {
             int randomIndex = _random.Next(i, toShuffle.Length);
 
-            T temp = toShuffle[i];
-            toShuffle[i] = toShuffle[randomIndex];
-            toShuffle[randomIndex] = temp;
+            T temp = toShuffle[randomIndex];
+            toShuffle[randomIndex] = toShuffle[i];
+            toShuffle[i] = temp;
         }
     }
 }
