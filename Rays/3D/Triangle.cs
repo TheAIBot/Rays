301a2410a374efa3763eb63849a832daf93aac98
TheAIBot <andreasuranus@hotmail.com>
2023-08-18 00:28:02 +0200

    Simplify cross platform triangle intersection code by utilizing Vector4 wherever possible

--- Rays/3D/Triangle.cs
+++ Rays/3D/Triangle.cs
@@ -15,31 +15,21 @@ public readonly record struct Triangle(Vector4 CornerA, Vector4 CornerB, Vector4
 
     public bool TryGetIntersection(Ray ray, out TriangleIntersection intersection)
     {
-        if (!Sse41.IsSupported)
-        {
-            return TryGetIntersectionCrossPlatform(ray, out intersection);
-        }
-
-        var a = CornerA.AsVector128();
-        var b = CornerB.AsVector128();
-        var c = CornerC.AsVector128();
-        var rayStart = ray.Start.AsVector128();
-        var rayDirection = ray.Direction.AsVector128();
-        Vector128<float> E1 = b - a;
-        Vector128<float> E2 = c - a;
-        Vector128<float> N = Cross(E1, E2);
-        float det = (-Sse41.DotProduct(rayDirection, N, 0b0111_0111)).GetElement(0);
+        Vector4 E1 = CornerB - CornerA;
+        Vector4 E2 = CornerC - CornerA;
+        Vector4 N = Cross(E1, E2);
+        float det = -Vector4.Dot(ray.Direction, N);
         if (det < 1e-6f)
         {
             intersection = default;
             return false;
         }
 
-        Vector128<float> AO = Sse.Subtract(rayStart, a);
-        Vector128<float> DAO = Cross(AO, rayDirection);
-        float u = (Sse41.DotProduct(E2, DAO, 0b0111_0111).GetElement(0));
-        float v = -(Sse41.DotProduct(E1, DAO, 0b0111_0111).GetElement(0));
-        float t = (Sse41.DotProduct(AO, N, 0b0111_0111).GetElement(0));
+        Vector4 AO = ray.Start - CornerA;
+        Vector4 DAO = Cross(AO, ray.Direction);
+        float u = Vector4.Dot(E2, DAO);
+        float v = -Vector4.Dot(E1, DAO);
+        float t = Vector4.Dot(AO, N);
         if (t < 0.0f ||
             u < 0.0f ||
             v < 0.0f)
@@ -53,34 +43,17 @@ public readonly record struct Triangle(Vector4 CornerA, Vector4 CornerB, Vector4
         u *= inversedet;
         v *= inversedet;
         intersection = new TriangleIntersection(t, u, v);
-        return ((u + v) <= 1.0f);
+        return (u + v) <= 1.0f;
     }
 
-    // https://stackoverflow.com/a/42752998
-    private bool TryGetIntersectionCrossPlatform(Ray ray, out TriangleIntersection intersection)
+    private static Vector4 Cross(Vector4 vector1, Vector4 vector2)
     {
-        Vector4 E1 = CornerB - CornerA;
-        Vector4 E2 = CornerC - CornerA;
-        Vector4 N = Vector3.Cross(E1.ToTruncatedVector3(), E2.ToTruncatedVector3()).ToZeroExtendedVector4();
-        float det = -Vector4.Dot(ray.Direction, N);
-        if (det < 1e-6f)
+        if (Sse.IsSupported)
         {
-            intersection = default;
-            return false;
+            return Cross(vector1.AsVector128(), vector2.AsVector128()).AsVector4();
         }
 
-        float invdet = 1.0f / det;
-        Vector4 AO = ray.Start - CornerA;
-        Vector4 DAO = Vector3.Cross(AO.ToTruncatedVector3(), ray.Direction.ToTruncatedVector3()).ToZeroExtendedVector4();
-        float u = Vector4.Dot(E2, DAO) * invdet;
-        float v = -Vector4.Dot(E1, DAO) * invdet;
-        float t = Vector4.Dot(AO, N) * invdet;
-
-        intersection = new TriangleIntersection(t, u, v);
-        return (t >= 0.0 &&
-                u >= 0.0 &&
-                v >= 0.0 &&
-                (u + v) <= 1.0);
+        return Vector3.Cross(vector1.ToTruncatedVector3(), vector2.ToTruncatedVector3()).ToZeroExtendedVector4();
     }
 
     //Method 5 from https://geometrian.com/programming/tutorials/cross-product/index.php
